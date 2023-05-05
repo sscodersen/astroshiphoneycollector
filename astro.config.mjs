7aa42aef1c6f0ca211b760f9bed6e6fb8dfb1fdc
sscodersen <marketing.businessblueprint@gmail.com>
2023-05-05 12:42:43 -0700

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,10 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  site: "https://www.nectarhub.xyz",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap()]
 });
