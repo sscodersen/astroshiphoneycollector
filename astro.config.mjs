5be6ee27deb7c356eb372447835c333b3a1ad670
socialtribexyz <msclubnft@gmail.com>
2023-07-19 07:48:30 -0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.nectarhub.xyz",
+  site: "https://blog.nectarhub.xyz",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   }), mdx(), sitemap()]
